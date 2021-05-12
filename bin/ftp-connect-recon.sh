99390b5b855d800813a04de55937480576ad7cdd
Seva <binarytrails@users.noreply.github.com>
2021-05-12 14:36:04 -0400

    bin: create test.txt for ftp-connect-recon

--- bin/ftp-connect-recon.sh
+++ bin/ftp-connect-recon.sh
@@ -1,8 +1,9 @@
 echo "touch test && ./ftp-connect-recon.sh <ip> <user> <password> <logfile>"
+touch test.txt
 echo "open $1 21
 user $2 $3
 status
-put test
-delete test
+put test.txt
+delete test.txt
 bye " | ftp -v -n >> $4
 echo >> $4
