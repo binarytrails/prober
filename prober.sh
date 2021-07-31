dc337d3d64b522b2823fab1c5db6fce7c7e8ebeb
Seva <binarytrails@users.noreply.github.com>
2021-07-31 09:52:38 -0400

    prober: add lolbas & weblogic scanner

--- prober.sh
+++ prober.sh
@@ -20,7 +20,7 @@ AVERAGE="y";
 #HEAVY="";
 
 # temp
-mkdir ./temp
+mkdir -p ./temp
 
 # [ex]ploits
 mkdir -p sploits/extra;
@@ -28,13 +28,19 @@ if [[ "$LIGHT" != "y" && "$LIGHT" != "n" ]]; then
     read -p "Do you want to clone sploit sources? [light] (y/n)? " LIGHT;
 fi
 if [ "$LIGHT" = "y" ]; then
-    git clone https://github.com/qazbnm456/awesome-cve-poc sploits/extra/cve-poc;
+    git clone https://github.com/qazbnm456/awesome-cve-poc sploits/extra/qazbnm456;
+    git clone https://github.com/NHPT/CVE-Exploit-Script sploits/extra/nhpt;
 fi
-if [[ "$AVERAGE" != "y" && "$AVERAGE" != "n" ]]; then
-    read -p "Do you want to clone more sploit sources? [average] (y/n)? " AVERAGE;
+#if [[ "$AVERAGE" != "y" && "$AVERAGE" != "n" ]]; then
+#    read -p "Do you want to clone more sploit sources? [average] (y/n)? " AVERAGE;
+#fi
+#if [ "$AVERAGE" = "y" ]; then
+#fi
+if [[ "$HEAVY" != "y" && "$HEAVY" != "n" ]]; then
+    read -p "Do you want to clone extra exploits? [heavy] (y/n)? " HEAVY;
 fi
-if [ "$AVERAGE" = "y" ]; then
-    git clone https://github.com/zhzyker/exphub sploits/extra/exphub;
+if [ "$HEAVY" = "y" ]; then
+    git clone https://github.com/zhzyker/exphub sploits/extra/zhzyker;
 fi
 
 # docs
@@ -48,6 +54,7 @@ if [ "$LIGHT" = "y" ]; then
     git clone https://github.com/GTFOBins/GTFOBins.github.io.git docs/gtfo-bins;
     wget https://raw.githubusercontent.com/snoopysecurity/awesome-burp-extensions/master/README.md -O docs/burp-extensions.md;
     wget https://gist.github.com/w00tc/486825a0b7c593789b1952878dd86ff5/archive/09ba2049b7d9737096cef161d5c5ddb22fe569dd.zip -O temp/w00tc-notes.zip && mkdir -p docs/w00tc && unzip -j ./temp/w00tc-notes.zip "486825a0b7c593789b1952878dd86ff5-09ba2049b7d9737096cef161d5c5ddb22fe569dd/*" -d docs/w00tc/; rm -rf ./temp/w00tc*;
+    git clone https://github.com/LOLBAS-Project/LOLBAS docs/living-off-the-land;
 fi
 if [[ "$AVERAGE" != "y" && "$AVERAGE" != "n" ]]; then
     read -p "Do you want to clone docs/pentest-book? [average] (y/n)? " AVERAGE;
@@ -75,7 +82,7 @@ if [[ "$LIGHT" != "y" && "$LIGHT" != "n" ]]; then
     read -p "Do you want to clone bin? [light] (y/n)? " LIGHT;
 fi
 if [ "$LIGHT" = "y" ]; then
-    wget https://raw.githubusercontent.com/eblazquez/fakelib.sh/master/fakelib.sh -O bin/fakelib.sh;
+    wget https://raw.githubusercontent.com/eblazquez/fakelib.sh/master/fakelib.sh -O bin/fakelib.sh && chmod +x bin/fakelib.sh;
     mkdir -p bin/nse;
     wget https://raw.githubusercontent.com/dolevf/nmap-graphql-introspection-nse/master/graphql-introspection.nse -O bin/nse/graphql-introspection.nse;
     git clone https://github.com/binarytrails/nmap-parse-output bin/nmap-parse-output;
@@ -98,7 +105,9 @@ if [ "$LIGHT" = "y" ]; then
     echo "go get github.com/tomnomnom/waybackurls" && go get github.com/tomnomnom/waybackurls;
       ln -fs ~/go/bin/waybackurls bin/waybackurls;
     git clone https://github.com/maldevel/EmailHarvester bin/email-harvester;
+    git clone https://github.com/0xn0ne/weblogicScanner bin/weblogic-scanner;
 fi
+
 
 # lists
 if [[ "$LIGHT" != "y" && "$LIGHT" != "n" ]]; then
