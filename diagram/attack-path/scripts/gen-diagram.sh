04d0676a5081bdfbc9a52eef801bc46d5c7ce34a
Seva <binarytrails@users.noreply.github.com>
2022-02-21 14:57:34 -0500

    diagram: add attack-path template

---
+++ diagram/attack-path/scripts/gen-diagram.sh
@@ -0,0 +1,3 @@
+# sudo npm install @mermaid-js/mermaid-cli -g
+mmdc -i sources/diagram.txt -b '#241F31' -o images/diagram.svg -c config.json
+mmdc -i sources/diagram.txt -b '#241F31' -o images/diagram.png -c config.json
