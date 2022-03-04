7bb98b64baad0f8e43606cfd73c3558d0821a5bb
Seva <binarytrails@users.noreply.github.com>
2022-03-04 18:25:54 -0500

    http_get: fix unconditional line change

--- bin/http/http_get_status_codes.sh
+++ bin/http/http_get_status_codes.sh
@@ -1,3 +1,3 @@
 # ./http_get_status_codes.sh list.txt
 for host in $(cat $1); do echo -n "$host " && \
-    curl -s -o /dev/null -w "%{http_code}" "$host" && echo; done
+    curl -s -o /dev/null -w "%{http_code}" "$host"; echo; done
