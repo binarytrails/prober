19625521defb09eea34a16a45e6fac605077041d
seva <5713600+binarytrails@users.noreply.github.com>
2023-07-13 15:47:14 +0000

    bin/ip: add subnet to ips script

---
+++ bin/ip/subnet2ips.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# Vsevolod Ivanov <seva@binarytrails.net>
+
+import sys, ipaddress
+subnet = sys.argv[1]
+ips = [str(ip) for ip in ipaddress.IPv4Network(subnet, strict=False)]
+print(ips)
